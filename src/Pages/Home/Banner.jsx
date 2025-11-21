653f015b084d32d7dfc0177c11ed5a5575f3fc76
farhanarexa <mailfarhana702@gmail.com>
2025-11-21 13:59:51 +0600

    logo route added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -7,20 +7,22 @@ import banner3 from '../../assets/banner/banner3.png';
 
 const Banner = () => {
     return (
-        <Carousel 
-        autoPlay={true}
-        infiniteLoop={true}
-        >
-            <div>
-                <img src={banner1} />
-            </div>
-            <div>
-                <img src={banner2} />
-            </div>
-            <div>
-                <img src={banner3} />
-           </div>
-        </Carousel>
+        <div className='mt-10'>
+            <Carousel
+                autoPlay={true}
+                infiniteLoop={true}
+            >
+                <div>
+                    <img src={banner1} />
+                </div>
+                <div>
+                    <img src={banner2} />
+                </div>
+                <div>
+                    <img src={banner3} />
+                </div>
+            </Carousel>
+        </div>
     );
 };
 
